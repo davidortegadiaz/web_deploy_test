1a5373e46c598ce210d698c7865cf320b4a63d4d
davidortegadiaz <jdortegad@gmail.com>
2025-07-08 16:35:18 +0000

    deploy: 51bf4877be269b15f4730ad70d4a9c5ad09fe4b8

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3318288293"
+    serviceWorkerVersion: "3862727987"
   }
 });
